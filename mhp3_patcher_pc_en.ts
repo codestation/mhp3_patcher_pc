dc5e6e0624668d1ad6ef11dc51be7066ff64f814
Sam Hegarty <hegarty.sam@gmail.com>
2011-10-17 02:37:59 +1300

    Add english translation

---
+++ mhp3_patcher_pc_en.ts
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="about.ui" line="14"/>
+        <source>Acerca de</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="72"/>
+        <source>Aceptar</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>Patcher</name>
+    <message>
+        <location filename="patcher.ui" line="26"/>
+        <source>MHP3rd patcher v1.4</source>
+        <translation>MHP3rd patcher v1.4</translation>
+    </message>
+    <message>
+        <location filename="patcher.ui" line="59"/>
+        <source>ISO</source>
+        <translation>ISO</translation>
+    </message>
+    <message>
+        <location filename="patcher.ui" line="82"/>
+        <location filename="patcher.ui" line="203"/>
+        <source>Buscar</source>
+        <translation>Browse</translation>
+    </message>
+    <message>
+        <location filename="patcher.ui" line="111"/>
+        <source>Parche</source>
+        <translation>Patch</translation>
+    </message>
+    <message>
+        <location filename="patcher.ui" line="153"/>
+        <location filename="patcher.ui" line="208"/>
+        <source>Parchear</source>
+        <translation>Apply Patch</translation>
+    </message>
+    <message>
+        <location filename="patcher.ui" line="173"/>
+        <source>Archivo</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="patcher.ui" line="182"/>
+        <source>Ayuda</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="patcher.ui" line="193"/>
+        <source>Acerca de</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="patcher.ui" line="198"/>
+        <source>Acerca de Qt</source>
+        <translation>About Qt</translation>
+    </message>
+    <message>
+        <location filename="patcher.ui" line="213"/>
+        <source>Salir</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="patcher.cpp" line="51"/>
+        <source>Abrir ISO</source>
+        <translation>ISO File</translation>
+    </message>
+    <message>
+        <location filename="patcher.cpp" line="51"/>
+        <source>Imagen iso (*.iso)</source>
+        <translation>ISO Image (*.iso)</translation>
+    </message>
+</context>
+</TS>
